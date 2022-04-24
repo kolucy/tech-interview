7c96038c604ea5a9b6aafe64381727c895d9d898
Boin Kim <charles1236@kau.kr>
2022-04-24 19:34:48 +0900

    Add: producer-consumer sychronization problem 예제 코드 추가

--- os/boin/code/fork-2019-08.c
+++ os/boin/code/fork-2019-08.c
@@ -4,6 +4,16 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
+  /*
+    main 함수의 매개변수 
+    - int argc : main 함수에 전달되는 정보의 개수
+    - char **argv : main 함수에 전달되는 정보, 문자열 타입의 배열 형식, 첫 번째 문자열은 프로그램의 실행경로로 고정
+  */
+  // for (int i = 0; i < argc; i++) // 옵션의 개수만큼 반복
+  // {
+  //   printf("%d : %s\n",argc, argv[i]); // 옵션 문자열 출력
+  // }
+
   int i = 0;
   int a;
   pid_t r; // pid_t 구조체 : 프로세스 고유의 id 값을 담기 위한 전용 구조체
@@ -27,7 +37,7 @@ int main(int argc, char **argv) {
       printf("c%d\n", i);
     } else {
       printf("x%d\n", i);
-      wait;
+      wait(NULL);
       execve("./a.out", args, NULL);
       printf("y%d\n", i);
     }
